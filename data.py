4c5051e049fc85eaf75f6597fa7436b0211b0912
szhang67 <30703276+szhang67@users.noreply.github.com>
2018-02-13 15:14:50 -0500

    Add files via upload

--- data.py
+++ data.py
@@ -1,10 +1,12 @@
-from os.path import exists, join, basename
+from os.path import exists, join, basename, abpath
 from os import listdir
 from scipy import interpolate
 from PIL import Image
 import cv2
 import numpy as np
 from numpy import array
+from random import shuffle
+from math import floor
 
 def input_transform(im, upscale_factor):
     im_size = im.size
@@ -21,14 +23,22 @@ def load_img(filename):
     y, Cb, Cr = img.split()
     return y
 
-root_dir = "/home/szhang67/data/raw_data"
-preprocessed_dir = "/home/szhang67/data/preprocessed"
+def data_preprocess(upscale_factor, root_dir, preprocessed_dir):
+    for x in listdir(root_dir):
+        if is_image_file(x):
+            filename = join(root_dir, x)
+            input = load_img(filename)
+            output = input_transform(input, upscale_factor)
+            cv2.imwrite(join(preprocessed_dir, x) + '.jpg', array(output))#check if this is okay and totensor
 
-upscale_factor = 4
-for x in listdir(root_dir):
-    if is_image_file(x):
-        filename = join(root_dir, x)
-        input = load_img(filename)
-        output = input_transform(input, upscale_factor)
-        cv2.imwrite(join(preprocessed_dir, x) + '.jpg', array(output))
-
+def train_test_split(upscale_factor, root_dir, preprocessed_dir, split_ratio):
+    data_preprocess(upscale_factor, root_dir, preprocessed_dir)
+    file_list = listdir(abpath(preprocessed_dir))
+    shuffle(file_list) #is it necessary since there is shuffle in dataloader
+    split_idx = floor(len(file_list) * split)
+    training = file_list[:split_idx]
+    testing = file_list[split_idx:]
+    return training, testing
+#where to put totensor()
+#do I need to save train and test data to a folder each time?
+#if justing doing image SR, how many train/test
